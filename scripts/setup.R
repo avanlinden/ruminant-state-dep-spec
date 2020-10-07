842ea1b35b03c109183560857a3e77ff97a40cb5
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-10-07 14:34:07 -0400

    setup script

---
+++ scripts/setup.R
@@ -0,0 +1,11 @@
+
+### Ruminant sexual dimorphism state-dependent speciation and extinction models
+### Abby Vander Linden
+### 2020-10-07
+
+### setup ===============
+
+library(tidyverse)
+library(ape)
+library(phytools)
+library(ggtree)
