761acbbfcb75139529146f89cc03072e3cbca824
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-10-30 07:15:51 -0400

    draft ape ace models

---
+++ scripts/ancestral-char-est.R
@@ -0,0 +1,19 @@
+ancestral state reconstruction
+
+#discrete models
+
+#even rates
+aceChen20_ER <- ace(x = chenSize$sd20, phy = chenTree, type = "discrete", marginal = TRUE)
+
+library(ggtree)
+
+## visualize ace models
+
+pal <- c("#F8766D", "#00BFC4")
+names(pal) 
+
+plot(chenTree, cex = 0.7, lwd = 1, label.offset = 1.3, main = "Single-Rate Discrete ACE")
+nodelabels(node=1:chenTree$Nnode+Ntip(chenTree),
+           pie = aceChen20_ER$lik.anc, piecol = c("black", "white"), cex = 0.5)
+tiplabels(pch = 15, col = )
+
