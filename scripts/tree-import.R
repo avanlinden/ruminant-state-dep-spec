dabdb716010b98b8d683103e15ac3311c08dfd50
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-11-25 07:20:22 -0500

    collapse duplicate subspecies, rename tips on Bibi 2013 mtgenome tree

--- scripts/tree-import.R
+++ scripts/tree-import.R
@@ -1,5 +1,7 @@
 
-### load ruminant whole-genome ML tree from Chen et al 2019 Science
+# import and inspect trees
+
+### load ruminant whole-genome ML tree from Chen et al 2019 Science =============
 ### (divergence times added manually from SI, originally estimated using r8s software)
 
 # load tree
@@ -35,5 +37,61 @@ chenTree <- drop.tip(chenTree, "Orcinus orca")
 write.nexus(chenTree, file = here("trees/tidy-Chen-tree.nex"))
 
 write.tree(chenTree, file = here("trees/tidy-chen-tree.tre"))
+
+
+### Bibi 2013 mitochondrial genome bovid tree =============
+
+fullBibiTree <- read.nexus(here("trees/bibi-mcc.nex"))
+
+## collapse subspecies and multiple specimen numbers per tip
+
+bibiTips <- as_tibble(fullBibiTree$tip.label)
+
+# bibiCounts <- bibiTips %>% 
+#   separate(value, into = c("genus", "species", "subspecies"), sep = "_", fill = "right") %>% 
+#   mutate(genSpec = str_c(genus, species, sep = "_")) %>% 
+#   count(genSpec, sort = TRUE)
+
+collapsedBibiTips <- bibiTips %>% 
+  separate(value, into = c("genus", "species", "subspecies", "id"), sep = "_", fill = "right") %>% 
+  mutate(genSpec = str_c(genus, species, sep = "_")) %>% 
+  distinct(genSpec, .keep_all = TRUE) %>% 
+  mutate(bibiName = case_when(
+    is.na(subspecies) ~ genSpec,
+    is.na(id) & !is.na(subspecies) ~ str_c(genSpec, subspecies, sep = "_"),
+    TRUE ~ str_c(genSpec, subspecies, id, sep = "_")
+  )) 
+
+
+dropBibiTips <- bibiTips %>% 
+  dplyr::filter(!value %in% collapsedBibiTips$bibiName) 
+
+
+# collapse Bibi tree to unique species
+
+
+bibiTree <- drop.tip(fullBibiTree, dropBibiTips$value)
+
+
+
+prunedBibiTips <- as_tibble(bibiTree$tip.label)
+
+
+prunedBibiGenSpec <- prunedBibiTips %>% 
+  separate(value, into = c("genus", "species", "subspecies", "id"), sep = "_", fill = "right") %>% 
+  mutate(genSpec = str_c(genus, species, sep = "_")) %>% 
+  select(genSpec)
+
+bibiTree$tip.label <- prunedBibiGenSpec$genSpec
+
+plot(bibiTree, cex = 0.7)
+
+#### use this tree for further analyses: ===============
+
+bibiTree
+
+write.nexus(bibiTree, file = here("trees/tidy-Bibi-tree.nex"))
+
+write.tree(bibiTree, file = here("trees/tidy-Bibi-tree.tre"))
 
 
