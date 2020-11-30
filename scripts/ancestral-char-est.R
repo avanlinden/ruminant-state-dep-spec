421d57670afda3eef3efe42ce838ba15d9e4a727
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-11-30 16:24:59 -0500

    visualize ape ace reconstructions for bibi tree

--- scripts/ancestral-char-est.R
+++ scripts/ancestral-char-est.R
@@ -17,3 +17,28 @@ nodelabels(node=1:chenTree$Nnode+Ntip(chenTree),
            pie = aceChen20_ER$lik.anc, piecol = c("black", "white"), cex = 0.5)
 tiplabels(pch = 15, col = )
 
+### even rates ace with bibi tree
+
+char <- as.factor(bibiData$sd20)
+
+names(char) <- bibiData$species
+
+
+aceBibi20_ER <- ace(char, bibiTree, type = "discrete", method = "ML", marginal = TRUE)
+
+
+plot(bibiTree, cex = 0.6, lwd = 1, label.offset = 1.3, main = "Single-Rate Discrete ACE")
+nodelabels(node=1:bibiTree$Nnode+Ntip(bibiTree),
+           pie = aceBibi20_ER$lik.anc, piecol = c("black", "white"), cex = 0.4)
+tiplabels(pch = 21, col = "black", bg = tipViz[match(bibiTree$tip.label, rownames(tipViz)), 2], cex = 1.5, offset =0.7)
+
+char
+
+charpal <- c("black", "white")
+names(charpal) <- c("dimorphic", "monomorphic")
+
+charColors <- charpal[charpal = char]
+
+tipViz <- cbind(char, charColors) 
+
+tipViz[,2]
