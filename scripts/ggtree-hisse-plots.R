932823f7a83a707053efc393ba2d7e759a17b282
Abby Vander Linden <11965371+avanlinden@users.noreply.github.com>
2020-12-08 19:56:27 -0500

    ggsave

--- scripts/ggtree-hisse-plots.R
+++ scripts/ggtree-hisse-plots.R
@@ -49,96 +49,87 @@ bibiNodes20 <- bibiProc20@data %>%
   mutate(monomorphic = 1 - dimorphic) %>% 
   as.data.frame()
 
-#get colors
+#get node pie colors
 
-#pieCols <- lacroix_palette("PeachPear", n = 2, type = "discrete")
 pieCols <- c("white", "black")
 names(pieCols) <- c("monomorphic", "dimorphic")
 
-#create pies
+#create node pies using my function which doesn't suck
 source(here("scripts/better-nodepie-function.R"))
 pies <- nodepie2(bibiNodes20, cols = 2:3, line_color = "black")
 pies <- lapply(pies, function(g) g + scale_fill_manual(values = pieCols))
 
-ggtree(bibiProc20@phylo, size = 0.25) +
-  geom_inset(pies, width = 0.04, height = 0.04, hjust = 0.1) +
-  theme_tree2()
-
-# gghisse rate tree
-
-lacroix_palettes$CranRaspberry[6]
-
-rateCols <- c(lacroix_palettes$CranRaspberry[1,6], lacroix_palettes$CranRaspberry[1,1])
-
-ratePlot <- h_rate_recon(bibiProc20,
-                         colors = rateCols)
-ratePlot +
-  geom_tree(size = 1.5) +
-  geom_inset(pies, width = 0.07, height = 0.07, hjust = 0.1) +
-  geom_tippoint(aes(color = state))
-  #scale_color_discrete(c("white", "black"))
-
-#get tip states
-
-bibiTipStates <- bibiProc20@data %>% 
-  slice(1:113) %>% 
-  select(id, state) %>% 
-  column_to_rownames(var = "id") %>% 
-  as.data.frame() 
-
-# fuck gghisse, fuck the ggtree documentation, just try from scratch
-
-#use model averaged treedata object
-class(bibiProc20)
-
+#designate tip colors
 tipCols = c("0" = "white", "1" = "black")
 
+#plot tree with branches colored by div rate, pies as insets, and states as tippoints
+
 ggtree(bibiProc20@phylo, aes(color = bibiProc20@data$net.div), size = 1.5) +
-  scale_color_gradientn(colors = rev(c(lacroix_palette("CranRaspberry", n =6, type = "continuous")))) +
-  geom_inset(pies, width = 0.03, height = 0.03, hjust = 0.1) +
+  scale_color_gradientn(colors = rev(c(
+    lacroix_palette("CranRaspberry", n = 6, type = "continuous")
+  ))) +
+  geom_inset(pies,
+             width = 0.03,
+             height = 0.03,
+             hjust = 0.1) +
   labs(color = "div rate") +
   ggnewscale::new_scale_color() +
-  geom_tippoint(size = 1.8, position = position_nudge(x = 1, y = 0.5)) +
+  geom_tippoint(size = 1.8, position = position_nudge(x = 0.5, y = 0)) +
   scale_fill_manual(values = "black") +
   ggnewscale::new_scale_color() +
-  geom_tippoint(aes(color = as.factor(bibiProc20@data$state)), size = 1, position = position_nudge(x = 1, y = 0.5)) +
+  geom_tippoint(aes(color = as.factor(bibiProc20@data$state)),
+                size = 1,
+                position = position_nudge(x = 0.5, y = 0)) +
   scale_color_manual(values = tipCols) +
-  labs(color = "state")
+  labs(color = "state", title = "Bibi Tree with 20% SexDimorph Ratio")
+
+ggsave(here("figures/bibi-sd20-ggtree-rates-pies.png"))
+
+### now do it with the chen tree =============
+
+# list all files in output directory 
+filePaths <- list.files(here("hisse-marginal-recons"), full.names = TRUE)
+
+#select files with full paths for all chen models
+chenRecPaths <- filePaths[str_detect(filePaths, "chen")]
+
+#separate list of files without full paths fornaming
+files <- list.files(here("hisse-marginal-recons"))
+
+#create vector of names for models
+chenRecNames <- files[str_detect(files, "chen")] %>% 
+  str_remove(".rds")
+
+#read in all chen tree RDS objects in directory 
+chenRecObjList <- chenRecPaths %>% 
+  map(readRDS)
+
+#store as a named list
+names(chenRecObjList) <- chenRecNames
+
+#separate based on sdratio
+
+chen_sd20RecList <-  chenRecObjList[chenRecNames[str_detect(chenRecNames, "sd20")]]
+chen_sd10RecList <- chenRecObjList[chenRecNames[str_detect(chenRecNames, "sd10")]]
+
+# gghisse processing, subset for ggtree
+
+chenProc20 <- h_process_recon(chen_sd20RecList)[[1]]
+
+#node data
+chenNodes20 <- chenProc20@data %>% 
+  slice(52:n()) %>% 
+  select(node = id, dimorphic = state) %>% 
+  mutate(monomorphic = 1 - dimorphic) %>% 
+  as.data.frame()
+#get node pie colors
+
+pieCols <- c("white", "black")
+names(pieCols) <- c("monomorphic", "dimorphic")
+
+#create node pies using my function which doesn't suck
+source(here("scripts/better-nodepie-function.R"))
+chen_pies <- nodepie2(chenNodes20, cols = 2:3, line_color = "black")
+chen_pies <- lapply(pies, function(g) g + scale_fill_manual(values = pieCols))
+
   
-
-# trying it with ape since for whatever reason can't do pies on circular tree
-
-library(ape)
-
-ape::plot.phylo(ladderize(bibiProc20@phylo), show.tip.label = FALSE, type = "fan")
-nodelabels(node = as.numeric(bibiNodes20$node),
-           pie = as.matrix(cbind(as.numeric(bibiNodes20[,2]), as.numeric(bibiNodes20[,3]))),
-           piecol = c("black", "white"), 
-           cex = 0.4)
-
-# add a contMap for div time
-
-bibiNodeRates20 <- bibiProc20@data %>% 
-  slice(114:n()) %>% 
-  select(id, net.div) %>%
-  column_to_rownames(var = "id") %>% 
-  as.data.frame() 
-
-### fuck it just runs an ape ACE anyway
-rateMap <- contMap(bibiProc20@phylo, bibiNodeRates20, plot = TRUE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
